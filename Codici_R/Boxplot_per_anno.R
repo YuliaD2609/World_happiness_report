86d039b9b45f1bbf97d81b7b8d18b84db7abcdee
vince <vincenzogenovese04@gmail.com>
2025-11-05 15:17:17 +0100

    Barplot valori mancanti e boxplot per anno

--- Codici_R/Boxplot_per_anno.R
+++ Codici_R/Boxplot_per_anno.R
@@ -1,22 +1,22 @@
-# 1. Caricamento del dataset
+#Caricamento del dataset
 df <- read.csv(file.choose(), header = TRUE, sep = ",")
 
-# 2. Rimuovo eventuali valori mancanti
+#Rimuovo eventuali valori mancanti
 df_box <- df[!is.na(df$happiness_score) & !is.na(df$year), ]
 
-# 3. Creazione del boxplot base
+#Boxplot
 boxplot(happiness_score ~ factor(year),
         data = df_box,
-        col = "#5611F7",
+        col = "#74c476",
         border = "#132B43",
-        outcol = "red",           # colore punti outlier
-        pch = 16,                 # forma outlier (punto pieno)
+        outcol = "blue",           
+        pch = 16,                 
         main = "Distribuzione del punteggio di felicità per anno",
         xlab = "Anno",
         ylab = "Punteggio di felicità (Happiness Score)",
         cex.lab = 1.1,
         cex.axis = 0.8,
-        las = 2)                  # ruota etichette anni per leggibilità
+        las = 2)              
 
 grid(nx = NA, ny = NULL, col = "gray80", lty = "dotted")
 title(main = "Distribuzione del punteggio di felicità per anno", font.main = 2)
