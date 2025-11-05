171388dd97831beb00a7c5d737f74f72fa5f9ddc
vince <vincenzogenovese04@gmail.com>
2025-11-05 16:23:35 +0100

    Felicità media

--- Codici_R/Felicita_media_50_paesi.R
+++ Codici_R/Felicita_media_50_paesi.R
@@ -1,4 +1,4 @@
-#creazione dataframe
+#Lettura dati
 df <- read.csv(file.choose(), header = TRUE, sep = ",")
 
 # Calcolo media, min e max per paese
@@ -7,7 +7,7 @@ happiness_summary <- aggregate(happiness_score ~ country, data = df,
                                                    min = min(x, na.rm = TRUE),
                                                    max = max(x, na.rm = TRUE)))
 
-# Trasforma in data frame leggibile
+#Trasformazione in dataframe leggibile
 happiness_summary <- data.frame(
   country = happiness_summary$country,
   mean = happiness_summary$happiness_score[, "mean"],
@@ -15,37 +15,39 @@ happiness_summary <- data.frame(
   max = happiness_summary$happiness_score[, "max"]
 )
 
-# Ordina per felicità media e prendi i primi 50
+#Selezione dei 50 paesi con felicità media più alta
 top50 <- happiness_summary[order(-happiness_summary$mean), ][1:50, ]
 
-# Imposta margini
-par(mar = c(8, 5, 4, 2))
+#Imposta margini (più spazio per i nomi sotto)
+par(mar = c(10, 5, 4, 2))   # margine inferiore aumentato
 
-# Calcolo limite superiore per evitare overflow
+#Definizione dell'intervallo Y (nessun overflow)
+y_min <- min(top50$min) - 0.2
 y_max <- max(top50$max) + 0.5
 
-# Disegno barplot con limite visivo
-bar_positions <- barplot(top50$mean,
-                         names.arg = top50$country,
-                         las = 2,
-                         col = "#56B117",
-                         ylim = c(min(top50$min) - 0.5, y_max),
-                         main = "Top 50 Paesi per Felicità Media (2005–2023)",
-                         ylab = "Happiness Score",
-                         cex.names = 0.8,
-                         border = "gray40")
+colors_green <- colorRampPalette(c("#00441b", "#238b45", "#74c476", "#c7e9c0", "#f7fcf5"))(nrow(missing_by_country_initial))
 
-# Linee continue per minimo e massimo
+#barplot
+bap_positions <- barplot(top50$mean,
+              names.arg = top50$country,
+              las = 2,
+              col = colors_green,
+              ylim = c(0, 9),
+              main = expression(bold("Top 50 Paesi per Felicità Media (2005–2023)")),
+              ylab = "Happiness Score",
+              cex.names = 0.8,
+              )
+
+#Aggiungi linee per min e max
 lines(bar_positions, top50$min, type = "o", col = "red", lwd = 2, pch = 19)
-lines(bar_positions, top50$max, type = "o", col = "green", lwd = 2, pch = 19)
+lines(bar_positions, top50$max, type = "o", col = "blue", lwd = 2, pch = 19)
 
-
-# Legenda
+#Legenda chiara e compatta
 legend("topleft",
        legend = c("Minimo", "Massimo"),
-       col = c("red", "green"),
-       pch = c(19, 19),
-       lty = c(1, 1),
-       lwd = c(2, 2),
+       col = c("red", "blue"),
+       pch = 19,
+       lty = 1,
+       lwd = 2,
        bty = "n")
 
