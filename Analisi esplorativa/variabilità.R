7c982475c8ffcdb5527d2aa9e88de74ab2745a65
your-id <y.dashchuk@studenti.unisa.it>
2025-11-10 19:27:43 +0100

    variabilità

---
+++ Analisi esplorativa/variabilità.R
@@ -0,0 +1,13 @@
+df <- read.csv(file.choose(), header = TRUE, sep = ",")
+
+# Varianza campionaria
+var <- var(df$happiness_score, na.rm = TRUE)
+var
+
+# Deviazione standard campionaria
+sd <- sd(df$happiness_score, na.rm = TRUE)
+sd
+
+# Coefficiente di variazione (%)
+cv <- (sd_h / mean(df$happiness_score, na.rm = TRUE)) * 100
+cv
