5b9c4cb8f9b5e5afbbdd23ff92a85fffb947c56a
your-id <y.dashchuk@studenti.unisa.it>
2025-12-08 18:19:53 +0100

    Regressione lineare multipla

--- RQ1/PCA.R
+++ RQ1/PCA.R
@@ -33,8 +33,18 @@ scores <- pca$x[,1:3]
 # matrice delle distanze
 dist_euclidea <- dist(X_complete, method = "euclidean")
 dist_euclidea[1:10] #si stampano solo le prime 10 per avere una visione iniziale dei valori
+mat_dist <- as.matrix(dist_euclidea)
 
-scores <- pca$x[, 1:3]
+# matrice di similarità
+mat_similarity <- 1 - mat_dist / max(mat_dist)
+round(mat_similarity[1:5, 1:5], 3)
+
+library(pheatmap)
+pheatmap(mat_similarity,
+         main = "Matrice delle similarità tra paesi",
+         color = colorRampPalette(c("white", "#74c476", "#00441b"))(100))
+
+
 
 df$cluster_pc <- NA
 df$cluster_pc[complete.cases(X)] <- cutree(hc_pc, k = 3)
@@ -46,3 +56,40 @@ pairs(scores[,1:3], col=df$cluster_pc[complete.cases(X)], pch=19, cex = 0.4)
 plot(scores[,1], scores[,2], col=df$cluster_pc[complete.cases(X)],
      pch=19, cex = 0.4, xlab="PC1", ylab="PC2", main="Cluster su PC")
 
+
+
+library(dplyr)
+library(ggplot2)
+
+# Calcola la media della felicità per cluster
+mean_happiness_cluster <- df %>%
+  filter(!is.na(cluster_pc)) %>%
+  group_by(cluster_pc) %>%
+  summarise(mean_happiness = mean(happiness_score, na.rm = TRUE))
+mean_happiness_cluster
+
+# Grafico a barre
+ggplot(mean_happiness_cluster, aes(x = as.factor(cluster_pc), y = mean_happiness, fill = as.factor(cluster_pc))) +
+  geom_bar(stat = "identity") +
+  labs(title = "Media della felicità per cluster", x = "Cluster", y = "Media Happiness Score") +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+library(dplyr)
+
+# Media delle variabili per cluster
+cluster_summary <- df %>%
+  filter(!is.na(cluster_pc)) %>%
+  group_by(cluster_pc) %>%
+  summarise(across(c(happiness_score,
+                     log_gdp_per_capita,
+                     social_support,
+                     healthy_life_expectancy_at_birth,
+                     freedom_to_make_life_choices,
+                     generosity,
+                     perceptions_of_corruption,
+                     positive_affect,
+                     negative_affect),
+                   mean, na.rm = TRUE))
+
+print(cluster_summary)
---
+++ RQ1/Regressione_Lineare_Multipla.R
@@ -0,0 +1,52 @@
+df <- read.csv(file.choose(), header = TRUE, sep = ",")
+
+colors_green <- colorRampPalette(c("#00441b", "#238b45", "#74c476", "#c7e9c0", "#f7fcf5"))(nrow(missing_by_country_initial))
+
+library(dplyr)
+#medie per ogni variabili
+df_mean <- df %>%
+  group_by(country) %>%
+  summarise(
+    happiness_score = mean(happiness_score, na.rm = TRUE),
+    log_gdp = mean(log_gdp_per_capita_sc, na.rm = TRUE),
+    social_support = mean(social_support_sc, na.rm = TRUE),
+    healthy_life = mean(healthy_life_expectancy_at_birth_sc, na.rm = TRUE),
+    freedom = mean(freedom_to_make_life_choices_sc, na.rm = TRUE),
+    generosity = mean(generosity_sc, na.rm = TRUE),
+    corruption = mean(perceptions_of_corruption_sc, na.rm = TRUE),
+    positive_affect = mean(positive_affect_sc, na.rm = TRUE),
+    negative_affect = mean(negative_affect_sc, na.rm = TRUE)
+  )
+
+model <- lm(happiness_score ~ 
+              log_gdp +
+              social_support +
+              healthy_life +
+              freedom +
+              generosity +
+              corruption +
+              positive_affect +
+              negative_affect,
+            data = df_mean)
+
+summary(model)
+
+# grafico dei coefficienti
+coef <- summary(model)$coefficients[-1,1]
+barplot(coef,
+        las=2,
+        col=colors_green,
+        main="Effetto delle variabili sulla felicità",
+        ylab="Coefficiente stimato",
+        cex.names = 0.7,)
+abline(h=0, col="black")
+
+plot(model$fitted.values, model$residuals,
+     xlab="Valori predetti",
+     ylab="Residui",
+     main="Valori residui vs predetti",
+     pch = 16,
+     cex = 0.5)
+abline(h=0, col="red")
+
+
