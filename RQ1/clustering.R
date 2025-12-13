1816c09ace7d94277783369e882ab98734a2421e
your-id <y.dashchuk@studenti.unisa.it>
2025-12-13 18:34:46 +0100

    Renaming

--- RQ1/clustering.R
+++ RQ1/clustering.R
@@ -1,5 +1,17 @@
 df <- read.csv(file.choose(), header = TRUE, sep = ",")
 
+library(pheatmap)
+all_sc <- c("log_gdp_per_capita_sc",
+            "social_support_sc",
+            "positive_affect_sc",
+            "negative_affect_sc",
+            "healthy_life_expectancy_at_birth_sc",
+            "freedom_to_make_life_choices_sc",
+            "generosity_sc",
+            "perceptions_of_corruption_sc",
+            "happiness_score_sc"
+)
+
 vars_sc <- c("log_gdp_per_capita_sc",
              "social_support_sc",
              "positive_affect_sc",
@@ -7,34 +19,366 @@ vars_sc <- c("log_gdp_per_capita_sc",
              "healthy_life_expectancy_at_birth_sc",
              "freedom_to_make_life_choices_sc",
              "generosity_sc",
-             "perceptions_of_corruption_sc",
-             "happiness_score")
+             "perceptions_of_corruption_sc"
+)
+df_country <- aggregate(df[, all_sc],
+                        by = list(country = df$country),
+                        FUN = mean)
+df_country <- df_country[complete.cases(X), ]
+X <- df_country[all_sc]
 
-# senza valori mancanti
-X <- df[vars_sc]
+# rimozione di righe con NA
 X_complete <- X[complete.cases(X), ]
-# matrice trasposta
-X_t <- t(X_complete)
+X_scaled <- scale(X_complete)
 
-# matrice di correlazione tra variabili
-mat_corr <- cor(X_t, use = "pairwise.complete.obs", method = "pearson")
+# matrice di correlazione
+mat_cor <- cor(X_scaled)
+round(mat_cor, 3)
+pheatmap(mat_cor,
+         main = "Matrice di correlazione",
+         fontsize = 10,
+         angle_col = 45)
 
-# clustering gerarchico sulle variabili
-dist_var <- dist(mat_corr, method = "euclidean")
-hc_var <- hclust(dist_var, method = "ward.D2")
+# senza la variabile happiness score
+X <- df_country[vars_sc]
+rownames(X) <- df_country$country
+X_complete <- X[complete.cases(X), ]
+X_scaled <- scale(X_complete)
+mat_cor <- cor(X_scaled)
+
+# calcolo PCA
+pca <- prcomp(X_scaled, center = TRUE, scale. = TRUE)
+summary(pca)
+# per determinare il contributo delle componenti
+round(pca$rotation, 3)
+# screeplot
+plot(pca, type = "l",  main="Screeplot PCA")
+
+# si prendono in considerazione i primi
+scores <- pca$x[,1:3]
+# matrice delle distanze
+dist_euclidea <- dist(scores, method = "euclidean")
+dist_euclidea[1:10] # si stampano solo le prime 10 per avere una visione iniziale dei valori
+mat_dist <- as.matrix(dist_euclidea)
+rownames(mat_dist) <- rownames(X_scaled)
+colnames(mat_dist) <- rownames(X_scaled)
+round(mat_dist[1:10, 1:10], 3)
+
+# matrice di similarità
+mat_similarity <- 1 - mat_dist / max(mat_dist)
+rownames(mat_similarity) <- rownames(X_scaled)
+colnames(mat_similarity) <- rownames(X_scaled)
+round(mat_similarity[1:10, 1:10], 3)
+
+# matrice di covarianza
+mat_cov <- cov(X_scaled)
+round(mat_cov, 3)
+pheatmap(mat_cov,
+         main = "Matrice di covarianza",
+         fontsize = 10,
+         angle_col = 45)
+
+# matrice di non omogeneità
+non_omogeneity <- apply(X_scaled, 1, var)
+summary(non_omogeneity)
+
+# clustering gerarchico
+hc <- hclust(dist_euclidea, method="complete")
+str(hc, list.len = nrow(hc$merge)*2, max.level = 5)
 
 # dendrogramma
-plot(hc_var, main = "Cluster delle variabili (in base alla correlazione con la felicità)",
-     xlab = "Variabili", sub = "", cex = 0.9)
+plot(hc, main="Dendrogramma clutering per paese",
+     xlab="Variabili", ylab="Distanza", cex=0.4)
+rect.hclust(hc, k=3, border="red")
 
-# taglio in 3 cluster
-clusters_var <- cutree(hc_var, k = 3)
-print(clusters_var)
+# calcolo cluster
+clusters <- cutree(hc, k=3)
+clusters
 
-# heatmap per visualizzare la correlazione tra le variabili
+plot(scores[,1], scores[,2],
+     col=clusters, pch=19, cex=0.6,
+     xlab="PC1", ylab="PC2",
+     main="Clusters")
+
+# aggiunta cluster
+df_country$cluster <- clusters
+# raggruppamento dei paesi in base al cluster di appartenenza
+# media di felicità per ogni cluster
+aggregate(df_country$happiness_score_sc,
+          by = list(cluster = df_country$cluster),
+          FUN = mean)
+
+# kmeans
+kmeans_res <- kmeans(scores, centers = 3, nstart = 1)
+kmeans_res
+clusters_km <- kmeans_res$cluster
+par(mfrow = c(1,2))
+plot(scores[,1], scores[,2],
+     col = clusters, pch = 19,cex=0.6,
+     main = "Clustering gerarchico",
+     xlab = "PC1", ylab = "PC2")
+
+plot(scores[,1], scores[,2],
+     col = clusters_km, pch = 19, cex=0.6,
+     main = "k-means",
+     xlab = "PC1", ylab = "PC2")
+
+par(mfrow = c(1,1))
+
+wcss_km <- kmeans_res$tot.withinss
+wcss_km
+bcss_km <- kmeans_res$betweenss
+bcss_km
+n <- nrow(scores)
+k <- 3
+
+ch_km <- (bcss_km / (k - 1)) / (wcss_km / (n - k))
+ch_km
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wcss <- sapply(1:10, function(k){kmeans(X_complete, center=k, nstart=10)$tot.withinss})
+plot(1:10, wcss, type="b", pch=19, frame=FALSE,
+     main = "Elbow method",
+     ylab="wcss",
+     xlab="clusters")
+
+
+
+
+
+
+
+cluster_means <- aggregate(
+  df_country[, vars_sc],
+  by = list(cluster = df_country$cluster),
+  FUN = mean
+)
+
+round(cluster_means, 3)
+
+overall_means <- colMeans(df_country[, vars_sc])
+cluster_diff <- sweep(cluster_means[, -1], 2, overall_means)
+
+round(cluster_diff, 3)
+
+clusters <- unique(df_country$cluster)
+
+for (k in clusters) {
+  cat("\nCluster", k, "\n")
+  
+  df_k <- df_country[df_country$cluster == k, ]
+  
+  cors <- cor(df_k[, vars_sc], df_k$happiness_score_sc)
+  print(round(cors, 3))
+}
+
+non_omogeneity_cluster <- aggregate(
+  apply(df_country[, vars_sc], 1, var),
+  by = list(cluster = df_country$cluster),
+  FUN = mean
+)
+
+non_omogeneity_cluster
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Clustering tra paesi
+X <- df[vars_sc]
+X_complete <- X[complete.cases(X), ]
+X_scaled <- scale(X_complete)
+
+# PCA sui paesi
+pca <- prcomp(X_scaled, scale.=TRUE)
+scores <- pca$x[,1:3]
+
+# distanza sui paesi
+dist_p <- dist(scores, method="euclidean")
+
+# clustering gerarchico
+hc_p <- hclust(dist_p, method="complete")
+
+plot(hc_p, main="Dendrogramma dei Paesi")
+rect.hclust(hc_p, k=3, border="red")
+
+
+clusters_paesi <- cutree(hc_p, k=3)
+country_clusters <- factor(country_clusters)
+plot(scores,
+     col = country_clusters,
+     pch = 19, cex = 0.6,
+     xlab = "PC1", ylab = "PC2",
+     main = "Cluster dei paesi nello spazio PCA")
+legend("topright", legend=levels(country_clusters),
+       col=1:3, pch=19)
+
+
+
+
+
+
+
+
+
+
+
+
+# funzione per calcolare la dimensione di ogni cluster durante l'agglomerazione
+cluster_sizes <- function(hc) {
+  n <- length(hc$order)
+  sizes <- rep(1, n)
+  size_vec <- numeric(n - 1)
+  
+  for (i in 1:(n - 1)) {
+    left <- hc$merge[i, 1]
+    right <- hc$merge[i, 2]
+    
+    size_left <- ifelse(left < 0, 1, size_vec[left])
+    size_right <- ifelse(right < 0, 1, size_vec[right])
+    
+    size_vec[i] <- size_left + size_right
+  }
+  
+  return(size_vec)
+}
+
+# calcolo dimensioni dei cluster
+sizes <- cluster_sizes(hc)
+
+# costruzione tabella
+aggl_table <- data.frame(
+  Passo = 1:(nrow(hc$merge)),
+  Unione = apply(hc$merge, 1, paste, collapse = "  "),
+  Distanza = round(hc$height, 4),
+  Dimensione_Cluster = sizes
+)
+
+print(aggl_table)
+
+
+
+
+
+# scatterplot dei cluster
+plot(scores[,1], scores[,2], col=df$cluster_pc[complete.cases(X)],
+     pch=19, cex = 0.4, xlab="PC1", ylab="PC2", main="Clusters")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# CLUSTERING
+# Si considerano PC1-PC3
+scores <- pca$x[,1:3]
+
+# matrice delle distanze
+dist_euclidea <- dist(X_complete, method = "euclidean")
+dist_euclidea[1:10] #si stampano solo le prime 10 per avere una visione iniziale dei valori
+mat_dist <- as.matrix(dist_euclidea)
+
+# matrice di similarità
+mat_similarity <- 1 - mat_dist / max(mat_dist)
+round(mat_similarity[1:5, 1:5], 3)
+
 library(pheatmap)
-pheatmap(mat_corr,
-         main = "Heatmap di correlazione tra le variabili",
-         color = colorRampPalette(c("white", "#74c476", "#00441b"))(100),
-         display_numbers = TRUE,
-         number_format = "%.2f")
+pheatmap(mat_similarity,
+         main = "Matrice delle similarità tra paesi",
+         color = colorRampPalette(c("white", "#74c476", "#00441b"))(100))
+
+
+
+df$cluster_pc <- NA
+df$cluster_pc[complete.cases(X)] <- cutree(hc_pc, k = 3)
+
+#scatterplot con tutti i cluster
+pairs(scores[,1:3], col=df$cluster_pc[complete.cases(X)], pch=19, cex = 0.4)
+
+# scatterplot dei cluster
+plot(scores[,1], scores[,2], col=df$cluster_pc[complete.cases(X)],
+     pch=19, cex = 0.4, xlab="PC1", ylab="PC2", main="Cluster su PC")
+
+
+
+library(dplyr)
+library(ggplot2)
+
+# Calcola la media della felicità per cluster
+mean_happiness_cluster <- df %>%
+  filter(!is.na(cluster_pc)) %>%
+  group_by(cluster_pc) %>%
+  summarise(mean_happiness = mean(happiness_score, na.rm = TRUE))
+mean_happiness_cluster
+
+# Grafico a barre
+ggplot(mean_happiness_cluster, aes(x = as.factor(cluster_pc), y = mean_happiness, fill = as.factor(cluster_pc))) +
+  geom_bar(stat = "identity") +
+  labs(title = "Media della felicità per cluster", x = "Cluster", y = "Media Happiness Score") +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+library(dplyr)
+
+# Media delle variabili per cluster
+cluster_summary <- df %>%
+  filter(!is.na(cluster_pc)) %>%
+  group_by(cluster_pc) %>%
+  summarise(across(c(happiness_score,
+                     log_gdp_per_capita,
+                     social_support,
+                     healthy_life_expectancy_at_birth,
+                     freedom_to_make_life_choices,
+                     generosity,
+                     perceptions_of_corruption,
+                     positive_affect,
+                     negative_affect),
+                   mean, na.rm = TRUE))
+
+print(cluster_summary)
